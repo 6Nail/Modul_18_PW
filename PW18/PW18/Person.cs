79a05a602270c480724b621c23403408b51a5760
Наиль Акчурин <golumvicerlid35@gmail.com>
2019-10-07 20:50:56 +0600

    init comit and create practic work

---
+++ PW18/PW18/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PW18
+{
+    [Serializable]
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string BirthYear { get; set; }
+    }
+}
---
+++ PW18/PW18/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using CsvHelper;
+
+namespace PW18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons;
+            using (var reader = new StreamReader("persons.csv"))
+            {
+                using (var csv = new CsvReader(reader))
+                {
+                    persons = csv.GetRecords<Person>().ToList();
+                }
+            }
+
+            var serializier = new SoapFormatter();
+
+            using (var stream = File.Create("1.txt"))
+            {
+                serializier.Serialize(stream, persons);
+            }
+
+
+        }
+    }
+}
